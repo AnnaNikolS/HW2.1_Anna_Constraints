4148578188566fd2ee9708f12e0d8ba5a6fa974f
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-02 03:46:29 +0300

    Initial Commit

---
+++ HW2.1_Anna_Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1_Anna_Constraints
+//
+//  Created by Анна on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
